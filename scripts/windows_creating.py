5e1d8f39cf644c80561e66b3cd36dc4e077f7758
Drodi <mitunlolka@gmail.com>
2021-06-24 04:03:15 +0300

    Update windows_creating.py

--- scripts/windows_creating.py
+++ scripts/windows_creating.py
@@ -7,7 +7,8 @@ from tkinter import ttk
 from tkinter import messagebox
 from db_interaction import adding_to_workers, adding_to_children, adding_to_otdeli, deleting, \
     save, back_attr, back_named_col, back_names_cond, back_many_cond, \
-    get_workers, get_children, get_deps
+    get_workers, get_children, get_deps, return_dict, remove_workers, \
+    remove_children, remove_otdeli, save_workers, save_children, save_otdeli
 from plotting import make_plot
 
 
@@ -459,15 +460,169 @@ def create_otchet_window(root, text, df):
         messagebox.showerror("Error", "Вызов несуществующего атрибута")
 
 def change_row(rt, content, db_name):
+    """
+    Создание окна для поиска записи по индексу
+    """
     for widget in content.winfo_children():
         widget.destroy()
 
     label = tk.Label(content, text='Редактирование и удаление записей')
-    num_label = tk.Label(content, text='Введите индекс:')
+    num_label = tk.Label(content, text='Введите индекс: ')
     index_entry = ttk.Entry(content, width=40)
-    add = tk.Button(content, text='Получить запись', width=20)
+    get_entry = tk.Button(content, text='Получить запись', width=20, command=lambda: show_entry(rt, content, index_entry.get(), db_name))
     content.grid(column=0, row=0)
     label.grid(column=0, row=0)
     num_label.grid(column=0, row=1)
     index_entry.grid(column=2, row=1)
-    add.grid(column=3, row=1)
+    get_entry.grid(column=3, row=1)
+
+def show_entry(rt, content, index, db_name):
+    """
+    Создание окна для мзменения или удаления записи
+    """
+    db = return_dict(db_name)
+    if db_name == 'Работники':
+        show_workers_entry(rt, content, index, db)
+    elif db_name == 'Дети работников':
+        show_children_entry(rt, content, index, db)
+    elif db_name == 'Отделы':
+        show_otdeli_entry(rt, content, index, db)
+
+def show_workers_entry(rt, content, index, db):
+    """
+    Создание окна для изменения или удаления записи в справочнике workers
+    """
+    for widget in content.winfo_children():
+        widget.destroy()
+
+    print(db.loc[int(index), :])
+    fio, birth, child, vac, dep, prof, pay = db.loc[int(index), :]
+
+    label = tk.Label(content, text='Запись под индексом {}'.format(index))
+    fio_label = tk.Label(content, text='ФИО')
+    birth_label = tk.Label(content, text='Дата рождения')
+    child_label = tk.Label(content, text='ФИО ребенка')
+    vac_label = tk.Label(content, text='Был ли вакцинирован')
+    dep_label = tk.Label(content, text='Номер департамента')
+    prof_label = tk.Label(content, text='Проффессия')
+    pay_label = tk.Label(content, text='Зарплата')
+    fio_entry = ttk.Entry(content, width=40)
+    birth_entry = ttk.Entry(content, width=40)
+    child_entry = ttk.Entry(content, width=40)
+    vac_entry = ttk.Entry(content, width=40)
+    dep_entry = ttk.Entry(content, width=40)
+    prof_entry = ttk.Entry(content, width=40)
+    pay_entry = ttk.Entry(content, width=40)
+
+    fio_entry.insert(0, fio)
+    birth_entry.insert(0, birth)
+    child_entry.insert(0, child)
+    vac_entry.insert(0, vac)
+    dep_entry.insert(0, dep)
+    prof_entry.insert(0, prof)
+    pay_entry.insert(0, pay)
+
+    saving = tk.Button(content, text='Сохранить запись', width=20,
+                    command=lambda: save_workers(index, fio_entry.get(), birth_entry.get(), child_entry.get(),
+                    vac_entry.get(), dep_entry.get(), prof_entry.get(), pay_entry.get()))
+    removing = tk.Button(content, text='Удалить запись', width=20,
+                    command=lambda: remove_workers(index))
+    content.grid(column=0, row=0)
+    label.grid(column=0, row=0, columnspan=3)
+    fio_label.grid(column=0, row=1)
+    birth_label.grid(column=0, row=2)
+    child_label.grid(column=0, row=3)
+    vac_label.grid(column=0, row=4)
+    dep_label.grid(column=0, row=5)
+    prof_label.grid(column=0, row=6)
+    pay_label.grid(column=0, row=7)
+    fio_entry.grid(column=3, row=1, columnspan=2)
+    birth_entry.grid(column=3, row=2, columnspan=2)
+    child_entry.grid(column=3, row=3, columnspan=2)
+    vac_entry.grid(column=3, row=4, columnspan=2)
+    dep_entry.grid(column=3, row=5, columnspan=2)
+    prof_entry.grid(column=3, row=6, columnspan=2)
+    pay_entry.grid(column=3, row=7, columnspan=2)
+    saving.grid(column=2, row=9, columnspan=2)
+    removing.grid(column=4, row=9, columnspan=2)
+
+def show_children_entry(rt, content, index, db):
+    """
+    Создание окна для изменения или удаления записи в справочнике children
+    """
+    for widget in content.winfo_children():
+        widget.destroy()
+
+    fio, birth, garden = db.loc[int(index), :]
+
+    label = tk.Label(content, text='Добавление, редактирование и удаление записей')
+    fio_label = tk.Label(content, text='ФИО ребенка')
+    birth_label = tk.Label(content, text='Дата рождения')
+    garden_label = tk.Label(content, text='Номер садика')
+    fio_entry = ttk.Entry(content, width=40)
+    birth_entry = ttk.Entry(content, width=40)
+    garden_entry = ttk.Entry(content, width=40)
+
+    fio_entry.insert(0, fio)
+    birth_entry.insert(0, birth)
+    garden_entry.insert(0, garden)
+
+    saving = tk.Button(content, text='Сохранить запись', width=20,
+                    command=lambda: save_children(index, fio_entry.get(),
+                                                       birth_entry.get(),
+                                                       garden_entry.get()))
+    removing = tk.Button(content, text='Удалить запись', width=20,
+                    command=lambda: remove_children(index))
+
+    content.grid(column=0, row=0)
+    label.grid(column=0, row=0, columnspan=3)
+    fio_label.grid(column=0, row=1)
+    birth_label.grid(column=0, row=2)
+    garden_label.grid(column=0, row=3)
+    fio_entry.grid(column=3, row=1, columnspan=2)
+    birth_entry.grid(column=3, row=2, columnspan=2)
+    garden_entry.grid(column=3, row=3, columnspan=2)
+    saving.grid(column=2, row=9, columnspan=2)
+    removing.grid(column=4, row=9, columnspan=2)
+
+def show_otdeli_entry(rt, content, index, db):
+    """
+    Создание окна для изменения или удаления записи в справочнике otdeli
+    """
+
+    for widget in content.winfo_children():
+        widget.destroy()
+
+    num, date, tel, num_workers = db.loc[int(index), :]
+
+    label = tk.Label(content, text='Добавление, редактирование и удаление записей')
+    num_label = tk.Label(content, text='Номер отдела')
+    date_label = tk.Label(content, text='Дата создания')
+    tel_label = tk.Label(content, text='Телефон')
+    num_workers_label = tk.Label(content, text='Количество сотрудников')
+    num_entry = ttk.Entry(content, width=40)
+    date_entry = ttk.Entry(content, width=40)
+    tel_entry = ttk.Entry(content, width=40)
+    num_workers_entry = ttk.Entry(content, width=40)
+
+    num_entry.insert(0, num)
+    date_entry.insert(0, date)
+    tel_entry.insert(0, tel)
+    num_workers_entry.insert(0, num_workers)
+
+    saving = tk.Button(content, text='Сохранить запись', width=20,
+                    command=lambda: save_otdeli(index, num_entry.get(), date_entry.get(), tel_entry.get(), num_workers_entry.get()))
+    removing = tk.Button(content, text='Удалить запись', width=20,
+                    command=lambda: remove_otdeli(index))
+
+    content.grid(column=0, row=0)
+    label.grid(column=0, row=0, columnspan=3)
+    num_label.grid(column=0, row=1)
+    date_label.grid(column=0, row=2)
+    tel_label.grid(column=0, row=3)
+    num_workers_label.grid(column=0, row=4)
+    num_entry.grid(column=3, row=1, columnspan=2)
+    date_entry.grid(column=3, row=2, columnspan=2)
+    tel_entry.grid(column=3, row=3, columnspan=2)
+    saving.grid(column=2, row=9, columnspan=2)
+    removing.grid(column=4, row=9, columnspan=2)
